9ee1ccfa4d2214a77aa59dd0f8c92b7dbbc9e5a7
annu933 <kashyapannu933@gmail.com>
2025-01-27 00:04:49 +0530

    navbar responsive

--- src/app/navbar/page.js
+++ src/app/navbar/page.js
@@ -39,6 +39,43 @@ export default function Navbar({ className }) {
     };
   }, [active]);
 
+  const socialLinks = [
+    { href: "https://www.facebook.com", icon: RiFacebookFill },
+    { href: "https://www.twitter.com", icon: FaXTwitter },
+    { href: "https://www.instagram.com", icon: FaInstagram },
+    { href: "https://www.youtube.com", icon: AiFillYoutube },
+  ];
+
+
+
+  const renderMenuItems = () => (
+    <>
+      <Link href={"#"}>
+        <MenuItem setActive={setActive} active={active} item="Home" />
+      </Link>
+      <Link href={"#about"}>
+        <MenuItem setActive={setActive} active={active} item="About" />
+      </Link>
+      <Link href={"#services"}>
+        <MenuItem setActive={setActive} active={active} item="Services" />
+      </Link>
+      <Link href={"#booking"}>
+        <MenuItem setActive={setActive} active={active} item="Booking" />
+      </Link>
+      <Link href={"#contact"}>
+        <MenuItem setActive={setActive} active={active} item="Contact Us" />
+      </Link>
+    </>
+  );
+
+  const renderActionButton = () => (
+    <a href="#contact">
+      <button className="px-4 py-2 rounded-md border border-black hover:border-white bg-orange hover:bg-blue text-black hover:text-white text-sm hover:shadow-[4px_4px_0px_0px_orange] transition duration-200">
+        Book Your Ticket
+      </button>
+    </a>
+  );
+
   return (
     <div className={cn("", className)}>
       <Menu setActive={setActive} className="gap-20 px-4">
@@ -47,16 +84,18 @@ export default function Navbar({ className }) {
           <ProductItem href="#" src="/images/logo.png" alt="logo" />
         </div>
         {/* Hamburger Menu for Mobile */}
-        <div className="lg:hidden">
+        <div className="lg:hidden z-1000">
           <button
-            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-            className="text-white focus:outline-none"
+           onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+           className="text-white focus:outline-none"
+           aria-expanded={isMobileMenuOpen}
           >
             {isMobileMenuOpen ? <FiX size={24} /> : <FiMenu size={24} />}
           </button>
         </div>
-        <div className="flex flex-col xl:w-full hidden lg:block xl:block">
-          <div className="flex justify-between bg-white px-5 py-3 ">
+        {/* Desktop Menu*/}
+        <div className="lg:flex flex-col xl:w-full hidden">
+          <div className="flex justify-between  bg-white px-5 py-3 ">
             <div className="flex ">
               <a href="mailto:jaimaatravels008@gmail.com">
                 <h5 className="text-gray lg:mr-2">
@@ -84,7 +123,7 @@ export default function Navbar({ className }) {
                 096348 75631
               </a>
             </div>
-            <div className="flex gap-4">
+            {/* <div className="flex gap-4 ">
               <a
                 href="https://www.facebook.com"
                 target="_blank"
@@ -113,14 +152,29 @@ export default function Navbar({ className }) {
               >
                 <AiFillYoutube color="orange" className="inline-block" />
               </a>
+            </div> */}
+
+            <div className="flex gap-4">
+              {socialLinks.map(({ href, icon: Icon }, index) => (
+                <a key={index} href={href} target="_blank" className="text-white">
+                  <Icon color="orange" className="inline-block" />
+                </a>
+              ))}
             </div>
           </div>
 
           <div className="flex justify-between px-5 py-4">
-            {/* Navbar Items */}
-            <div
+            <div className="flex gap-8 lg:items-center">{renderMenuItems()}</div>
+            <div className="hidden lg:block">{renderActionButton()}</div>
+          </div>
+        </div>
+      </Menu>
+        {/* Mobile Menu */}
+      <Menu className="flex ">
+        <div className="flex justify-between items-center px-5 lg:hidden xl:hidden">
+          <div
               className={cn(
-                "flex-col lg:flex-row lg:flex gap-8 lg:items-center lg:justify-center lg:static lg:visible",
+                "flex-col lg:flex-row lg:flex gap-8 lg:items-center lg:justify-center lg:static lg:visible z-50",
                 {
                   "flex absolute top-[100%] left-0 w-full bg-blue text-white shadow-lg p-4 lg:p-0 lg:bg-transparent lg:shadow-none":
                     isMobileMenuOpen,
@@ -128,75 +182,13 @@ export default function Navbar({ className }) {
                 }
               )}
             >
-              <Link href={"#"}>
-                <MenuItem setActive={setActive} active={active} item="Home" />
-              </Link>
-              <Link href={"#about"}>
-                <MenuItem setActive={setActive} active={active} item="About" />
-              </Link>
-
-              {/* <div
-            ref={dropdownRef}
-            onClick={() =>
-              setActive(active === "Our Services" ? null : "Our Services")
-            }
-          >
-            <MenuItem setActive={setActive} active={active} item="Our Services">
-              {active === "Our Services" && (
-                <div className="flex flex-col space-y-4 text-sm text-center">
-                  <HoveredLink href="/services">All Services</HoveredLink>
-                  <HoveredLink href="/booking">All Booking</HoveredLink>
-                  <HoveredLink href="/blog">All Blog</HoveredLink>
-                  <HoveredLink href="/services">All Services</HoveredLink>
-                </div>
-              )}
-            </MenuItem>
-          </div> */}
-              <Link href={"#services"}>
-                <MenuItem
-                  setActive={setActive}
-                  active={active}
-                  item="Services"
-                />
-              </Link>
-              <Link href={"#booking"}>
-                <MenuItem
-                  setActive={setActive}
-                  active={active}
-                  item="Booking"
-                />
-              </Link>
-              <Link href={"#contact"}>
-                <MenuItem
-                  setActive={setActive}
-                  active={active}
-                  item="Contact Us"
-                />
-              </Link>
+              {/* Menu Items */}
+              {renderMenuItems()}
 
               {/* Action Buttons */}
               <div className="flex gap-4 lg:ml-4   sm:block  lg:hidden">
-                <a href="#contact">
-                  <button className="signup px-4 py-2 rounded-md border border-black hover:border-white bg-orange hover:bg-blue text-black hover:text-white text-sm hover:shadow-[4px_4px_0px_0px_orange] transition duration-200">
-                    Book You Ticket
-                  </button>
-                </a>
-                {/* <button className="px-4 py-2 rounded-md border border-black bg-white text-black text-sm hover:shadow-[4px_4px_0px_0px_orange] transition duration-200">
-                  Login
-                </button> */}
+              {renderActionButton()}
               </div>
-            </div>
-            {/* Action Buttons for desktop side */}
-            <div className="  lg:ml-4 hidden lg:block">
-              <a href="#contact">
-                <button className=" px-4 py-2 rounded-md border border-black hover:border-white bg-orange hover:bg-blue text-black hover:text-white text-sm hover:shadow-[4px_4px_0px_0px_orange] transition duration-200 mr-3">
-                  Book You Ticket
-                </button>
-              </a>
-              {/* <button className="px-4 py-2 rounded-md border border-black bg-white text-black text-sm hover:shadow-[4px_4px_0px_0px_orange] transition duration-200">
-                Login
-              </button> */}
-            </div>
           </div>
         </div>
       </Menu>
